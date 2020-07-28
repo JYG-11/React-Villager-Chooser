d10df98ccc8439b2b92bb14d18a73371b57e70fd
JessieYG11 <yueguan1110@gmail.com>
2020-07-28 15:37:39 +0200

    :art: Refactored database fields

--- backend/routes/villagers.js
+++ backend/routes/villagers.js
@@ -10,31 +10,27 @@ router.route("/").get((req, res) => {
 router.route("/add").post((req, res) => {
   const name = req.body.name;
   const personality = req.body.personality;
-  const species = req.body.species;
+  const specie = req.body.specie;
   const birthday = Date.parse(req.body.birthday);
   const catchphrase = req.body.catchphrase;
   const skill = req.body.skill;
   const goal = req.body.goal;
   const fear = req.body.fear;
-  const style1 = req.body.style1;
-  const style2 = req.body.style2;
-  const color1 = req.body.color1;
-  const color2 = req.body.color2;
+  const styles = req.body.styles;
+  const colors = req.body.colors;
   const song = req.body.song;
 
   const newVillager = new Villager({
     name,
     personality,
-    species,
+    specie,
     birthday,
     catchphrase,
     skill,
     goal,
     fear,
-    style1,
-    style2,
-    color1,
-    color2,
+    styles,
+    colors,
     song,
   });
 
